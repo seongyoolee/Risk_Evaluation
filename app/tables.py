1000b6ba05dbe4471507f66170aa6ce8687f8970
Seon Lee <seon.lee@cfacorp.com>
2020-05-16 18:01:29 -0400

    add dollar sign

--- app/tables.py
+++ app/tables.py
@@ -1,5 +1,9 @@
 from flask_table import Table, Col, LinkCol
- 
+
+class DollarCol(Col):
+    def td_format(self, content):
+        return '$ ' + str(content)
+
 class InjuryClaimTable(Table):
     id = Col('Id', show=False)
     company = Col('Company', show=False)
@@ -7,7 +11,7 @@ class InjuryClaimTable(Table):
     injury_cause = Col('Injury Cause')
     open_or_closed = Col('Open/Closed')
     year = Col('Year')
-    incurred_loss = Col('Incurred Loss')
-    paid_loss = Col('Paid Loss')
+    incurred_loss = DollarCol('Incurred Loss')
+    paid_loss = DollarCol('Paid Loss')
     description = Col('Description')
     edit = LinkCol('Edit', 'edit', url_kwargs=dict(id='id'))
